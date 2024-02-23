dafd0618817823daca240306b6078b28c52d1cf4
Mac Mini <macmini@macs-mbp.myfiosgateway.com>
2024-02-23 12:25:55 -0500

    Add a new container to display the final result after 5 rounds. Add a function to refresh the current webpage if the players click any button after 5 rounds.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -35,17 +35,23 @@ function playRound(playerSelection, computerSelection) {
 
 // creates a new div referenced in the variable 'div'
 const container = document.querySelector('#container');
-
-// buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll('button');
+const final_score = document.createElement('div')
+final_score.style.color = 'blue'
+final_score.style.fontSize = '24px'
 const score1 = document.createElement('div') 
 const score2 = document.createElement('div')
+
+let round = 0
+let computer_score = 0
+let player_score = 0
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
   // and for each one we add a 'click' listener
-    computer_score = 0
-    player_score = 0
     button.addEventListener('click', () => {
+        round += 1
+        if (round > 5) {location.reload()}
         const computerSelection = getComputerChoice()
         let result = playRound(button.id, computerSelection)
         if (result == 'playerWin'){
@@ -56,6 +62,17 @@ buttons.forEach((button) => {
         score2.textContent = 'Computer Score: ' + computer_score
         container.appendChild(score1)
         container.appendChild(score2)
+        if (round == 5){
+            if (player_score > computer_score){
+                final_score.textContent = 'Player wins!'
+            }else if (player_score < computer_score){
+                final_score.textContent = 'Computer wins!'
+            }else{
+                final_score.textContent = 'A draw!'
+            }
+        container.appendChild(final_score)
+        
+        }
     });
 });
 
